1216c6a208017018b68188a9fef7c6dfe1de1306
cleivas <clara.leivas@gmail.com>
2020-11-26 09:35:08 +0100

    Update app.js

--- v. 48/Prototype/app.js
+++ v. 48/Prototype/app.js
@@ -13,7 +13,10 @@ console.log(arr); //Om man kollar under prototype hittar man alla instance-metod
 console.log(typeof arr); //instans av det globala Array-objektet, är ett objekt
 
 console.log(arr.push(10)); //vi kan använda alla instance-metoder på alla våra arrayer
-console.log(Array.push(10));
+console.log(Array.push(10)); //Fungerar inte eftersom push är en instance-metod, inte en statisk metod. Går alltså inte att använda på det globala Array-objektet 
+
+let arr2 = [9, 8, 7, 6];
+console.log(arr2, arr);
 
 
 
