acb920e9ffe3377da6f0bef267df82d78ce7747f
cleivas <clara.leivas@gmail.com>
2020-11-26 09:21:28 +0100

    added prototype examples

---
+++ v. 48/Prototype/app.js
@@ -0,0 +1,34 @@
+//2.a
+console.log(Math); //Är ett objekt, vi kan se alla metoder och egenskaper
+console.log(Math.random()); //Statiska metoder används 'direkt på objektet
+
+
+//2.b
+//är en funktion, vilket är en konstruktor
+//vi ser de statiska metoderna men inte instance metoderna 
+console.log(Array); 
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(arr); //Om man kollar under prototype hittar man alla instance-metoder som tillhör Array-objektet
+console.log(typeof arr); //instans av det globala Array-objektet, är ett objekt
+
+console.log(arr.push(10)); //vi kan använda alla instance-metoder på alla våra arrayer
+console.log(Array.push(10));
+
+
+
+
+/* let math = {
+    PI: 3.14,
+
+    double: function(x){
+        return x*2;
+    }
+}
+
+function array(){
+    console.log('i arrayfunktionen');
+}
+
+console.log(math);
+console.log(array); */
