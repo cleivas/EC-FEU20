fdfd310bb67ee4927ef724157430249745766611
cleivas <clara.leivas@gmail.com>
2020-11-26 12:53:32 +0100

    Added djur

---
+++ v. 48/Djur/app.js
@@ -0,0 +1,48 @@
+//jag behöver flera olika fyrbenta däggdjur
+//Eftersom de har många ilknande egenskaper vill jag inte behöva upprepa definitionerna
+
+function Animal(_sound){
+    //this.legs = 4; moved to an instance property
+    //this.tail = true;
+    this.sound = _sound;
+}
+
+Animal.prototype.legs = 4;
+Animal.prototype.tail = true;
+Animal.prototype.makeSound = function(){
+    console.log(this.sound);
+}
+
+let cat = new Animal('mjau');
+let wolf = new Animal('aaaoooouuuuu');
+let elephant = new Animal('tuuut tuuut');
+console.log(cat, wolf);
+
+cat.makeSound();
+wolf.makeSound();
+elephant.makeSound();
+
+//OM vi ska programmera en hel djurpark kommer detta bli jobbigt
+//Alla djur har fyra ben, och alla djur har svans
+/* let katt = {
+    antalBen: 4,
+    svans: true,
+    ljud: 'mjau',
+
+    prata: function(){
+        console.log(this.ljud);
+    }
+}
+
+let varg = {
+    antalBen: 4,
+    svans: true,
+    ljud: 'aaaoooouuuuu',
+    prata:function(){
+        console.log(this.ljud);
+    }
+}
+
+console.log(katt.antalBen, katt.svans);
+katt.prata();
+varg.prata(); */
--- v. 48/Prototype/createPrototypeMethods.js
+++ v. 48/Prototype/createPrototypeMethods.js
@@ -23,3 +23,15 @@ arr2.logLength();//'this' ersätts med 'arr2'
 //En instance-egenskap
 Array.prototype.namn = 'Clara';
 console.log(arr.namn);
+
+//String-objektet
+console.log(String);
+let str = 'Det här är en string';
+let str2 ='Det här är en helt annan string med en annan längd';
+
+String.prototype.logStringLength = function(){
+    console.log('Längden på stringen är:', this.length);
+}
+
+str.logStringLength();
+str2.logStringLength();
